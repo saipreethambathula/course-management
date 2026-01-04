7229bb594a59440ab74eee445e23bc3cf3d3adf0
saipreethambathula <saipreethambathula@saipreethambathulas-MacBook-Air.local>
2026-01-04 20:27:53 +0530

    updated env and config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require("sequelize");
+const path = require("path");
+
+const isProd = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./database.sqlite",
+  storage: isProd
+    ? path.join("/tmp", "database.sqlite") // Writable on Render
+    : path.join(__dirname, "database.sqlite"), // Local dev
   logging: false,
 });
 
